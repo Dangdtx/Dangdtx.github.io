723143b00ebae0d0bf7d50a4d496010a18c78c40
党党 <862294826@qq.com>
2022-10-10 18:09:43 +0800

    style(custom): 控制style

--- docs/guide/test.js
+++ docs/guide/test.js
@@ -1,15 +1,15 @@
 const sum = (a, b) => {
-  return a + b
-}
-let a = 'b'
-let c = 'c'
-let d = 'd'
-let e = 'e'
-let f = 'f'
+  return a + b;
+};
+let a = 'b';
+let c = 'c';
+let d = 'd';
+let e = 'e';
+let f = 'f';
 function direction(dir) {
-  console.log(dir)
+  console.log(dir);
 }
 
 function docs(wen) {
-  console.log(wen)
+  console.log(wen);
 }
