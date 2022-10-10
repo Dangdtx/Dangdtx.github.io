89e6e27a5797d3ce5fdb0aa2bee3250328ab45bd
党党 <862294826@qq.com>
2022-10-10 18:07:12 +0800

    dang

--- docs/guide/test.js
+++ docs/guide/test.js
@@ -1,14 +1,14 @@
 const sum = (a, b) => {
-  return a + b;
-};
-let a = "b";
-let c = "c";
-let d = "d";
-let e = "e";
+  return a + b
+}
+let a = 'b'
+let c = 'c'
+let d = 'd'
+let e = 'e'
 function direction(dir) {
-  console.log(dir);
+  console.log(dir)
 }
 
 function docs(wen) {
-  console.log(wen);
+  console.log(wen)
 }
