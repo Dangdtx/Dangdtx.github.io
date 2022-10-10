1b56a705dbe45f7bea1dbb32559785da2c90d580
党党 <862294826@qq.com>
2022-10-10 17:01:43 +0800

    dang

--- docs/config.js
+++ docs/config.js
@@ -1,11 +1,12 @@
 module.exports = {
     // 网站的一些基本配置
     // base:配置部署站点的基础路径，后续再介绍
-    title: '党党title', // 网站的标题
+    title: '党党title标题', // 网站的标题
     description: '党党测试网页', // 网站的描述，它将会以 <meta> 标签渲染到当前页面的 HTML 中。
     head: [
         ['link', { rel: 'icon', href: 'image/1.jpg' }] // 需要被注入到当前页面的 HTML <head> 中的标签
     ],
+
     themeConfig: {
         logo: 'image/1.jpg',
     }
